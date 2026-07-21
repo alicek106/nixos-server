956d98d011e4cf54094d2bdbe041ff16e5ad571b
alicek106 <chanho.yong@devsisters.com>
2026-07-22 02:09:20 +0900

    rekey secrets to new host key

--- nixos/secrets/secrets.nix
+++ nixos/secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   alice = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH31VMIW5aeAgjJXlGPD69Zs00NPrQ8pOwkLTJDJXC2x nixos-alicek106";
-  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/zQ2NhMyjUZU71p8igdJNwFwz/kl4tijR2x+QOqDvK root@nixos-alicek106"; # 서버 밀때마다 갈아줘야 함
+  server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjDFC4ffES5BhsYNS8qNu3WTbxpvm7schvJvxUlkY9r root@nixos-alicek106"; # 서버 밀때마다 갈아줘야 함
   all = [ alice server ];
 in
 {
